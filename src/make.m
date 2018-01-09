b3783accc6607b2cbf994c0ac5564e0bfce5fd05
Tiankui Zhang <tiankuizhang@email.arizona.edu>
2018-01-09 16:27:53 -0700

    compiler option change

--- src/make.m
+++ src/make.m
@@ -1,6 +1,17 @@
 % script to compile 
 
+% -v : display detailed build and troubleshooting information
+% -output nameofmex : control name of the mex file (for instance: mex -output mymex myfunc.c)
+% -outdir path_to_dir : control the path to output dir
+% to add -Wall flag, use: mex -v COMPFLAGS-'$cCOMPFLAGS -Wall' yprime.c
+% -n : preview without executing 
+% -c : compiler object file only
+% -O : optimizes the object code
+
 f1 = fullfile('mexReinitialization','mexReinitialization.c');
 f2 = fullfile('mexReinitialization','Reinitialization.c');
 
-mex(f1,f2)
+%mex(f1,f2)
+
+compiler_option = '/GL /fp:fast';
+mex(f1,f2,['COMPFLAGS="$COMPFLAGS ' compiler_option '"']);
